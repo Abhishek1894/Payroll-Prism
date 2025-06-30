055805e96054537c07ad71bc21414b9032aff755
abhishek-ainapur1894 <abiainapur24@gmail.com>
2025-07-01 04:30:02 +0530

    database configuration done, employee relation defined

---
+++ src/main/java/com/markTech/payrollPrism/model/Employee.java
@@ -0,0 +1,38 @@
+package com.markTech.payrollPrism.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private boolean active;
+
+}
