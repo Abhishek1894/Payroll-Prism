d8c5c868a8880510b5c51f1ed474000693f3a142
abhishek-ainapur1894 <abiainapur24@gmail.com>
2025-07-01 03:22:53 +0530

    paryroll prism started

---
+++ src/main/java/com/markTech/payrollPrism/PayrollPrismApplication.java
@@ -0,0 +1,15 @@
+package com.markTech.payrollPrism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollPrismApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(PayrollPrismApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/markTech/payrollPrism/PayrollPrismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markTech.payrollPrism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollPrismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
